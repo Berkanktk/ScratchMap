cde41b48d0760fe056e447faa884c1a9b565e34e
Berkanktk <berkank@live.dk>
2023-02-21 23:23:00 +0100

    Transferred most of the files to angular

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+// @ts-ignore
+import jsVectorMap from 'jsvectormap'
+import 'jsvectormap/dist/maps/world.js'
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent {
+  private map: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.map = new jsVectorMap({
+      selector: '#map',
+      map: 'world',
+      enableZoom: true,
+      showZoomButtons: true,
+      showTooltip: true,
+      regionsSelectable: true,
+      onRegionTooltipShow(event: any, tooltip: { css: (arg0: { backgroundColor: string; }) => void; }) {
+        tooltip.css({backgroundColor: '#c96'})
+      },
+      regionStyle: {
+        initial: {
+          fill: '#ffffff',
+        },
+        selected: {
+          fill: '#c96',
+        }
+      },
+      markerColor: '#c96',
+    })
+  }
+
+}
